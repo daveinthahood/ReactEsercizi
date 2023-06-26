94ef809502a8f39d41c2e63a775e23cd0883bba1
Dave <101344486+daveinthahood@users.noreply.github.com>
2023-06-26 22:06:22 +0200

    work

---
+++ Esercizio Numero Due/react2/src/main.jsx
@@ -0,0 +1,10 @@
+
+import ReactDOM from 'react-dom/client'
+import { Welcome } from './welcome'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+<>
+  <Welcome name= "Davide" />
+</>
+)
