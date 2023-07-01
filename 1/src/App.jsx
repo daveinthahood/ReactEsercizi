1a81347b8db67b2ee02f27a1801464372461a963
Dave <101344486+daveinthahood@users.noreply.github.com>
2023-07-01 22:28:14 +0200

    d

---
+++ 1/src/App.jsx
@@ -0,0 +1,32 @@
+import { useCallback, useState } from 'react'
+import React from 'react'
+
+
+
+class Counter extends React.Component {
+  constructor(props) {
+      super(props)
+      this.state = {
+          count: 0,
+          incrementAmount:1,
+          incrementInverval:1000
+      }
+      setInterval(() => {
+          this.setState((state) => {
+              return {
+                  count: state.count + this.state.incrementAmount,
+              }
+          })
+      }, this.state.incrementInverval)
+  }
+  render() {
+      return (
+          <div>
+              <h1>Counter: {this.state.count}</h1>
+          </div>
+      )
+
+  }
+}
+
+export default Counter
