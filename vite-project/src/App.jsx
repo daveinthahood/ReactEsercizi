460251ac7166d49f21132d79b79514498b16a0e9
Dave <101344486+daveinthahood@users.noreply.github.com>
2023-07-01 21:52:23 +0200

    done

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,30 @@
+import { useCallback, useState } from 'react'
+import React from 'react'
+
+
+
+class Counter extends React.Component {
+  constructor(props) {
+      super(props)
+      this.state = {
+          counter: 0
+      }
+      setInterval(() => {
+          this.setState((state) => {
+              return {
+                  counter: state.counter + 1,
+              }
+          })
+      }, 1000)
+  }
+  render() {
+      return (
+          <div>
+              <h1>Count: {this.state.counter}</h1>
+          </div>
+      )
+
+  }
+}
+
+export default Counter
