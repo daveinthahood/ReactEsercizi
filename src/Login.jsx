6bb7387cc3a8ba8e7deb349df80643a057a4e1e3
Dave <101344486+daveinthahood@users.noreply.github.com>
2023-07-08 11:15:51 +0200

    done

---
+++ src/Login.jsx
@@ -0,0 +1,36 @@
+const { useState } = require("react")
+const { Form } = require("react-router-dom")
+
+
+
+const Login = () => {
+    const [login, setLogin] = useState({
+        username: "",
+        password:"",
+    })
+
+
+    const handleInput = (event) => {
+        const {name, value} = event.target;
+        setLogin((_login) => {
+            return {
+                ..._login,
+                [name]:value,
+            }
+        })
+    }
+}
+
+return (
+    <>
+        <Form onSubmit={handleSubmit}>
+            <input type="text" name="username" value="{login.username}" onInput={handleInput}/>
+            <input type="password" name="password" value={login.password} onInput={handleInput}/>
+        </Form>
+        {login.name === "" || login.password === "" ? (<button disabled={true}> Submit</button>)
+        : (<button> Login</button>)        
+    
+    }
+        
+    </>
+)
