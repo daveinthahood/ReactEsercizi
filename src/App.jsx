6acbb7386e928b1dd4a84839c9ab9fa1b2955b3d
daveinthahood <davidesimone96@outlook.it>
2023-07-10 13:59:31 +0200

    d

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App =()=> {
+  
+  return (
+    <>
+      const name = <strong> Davide </strong>
+
+      <h1> 
+       Welcome {name}
+      </h1>
+      
+    </>
+  )
+}
+
+export default App
