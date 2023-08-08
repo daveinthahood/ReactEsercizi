30c225e7c9b401df6300e7941e4502fd64351e2d
daveinthahood <davidesimone96@outlook.it>
2023-08-08 12:20:09 +0200

    d

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { GitHubUsers } from './GitHubUsers'
+import { ClickTracker } from './ClickTracker'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <GitHubUsers />
+  <ClickTracker />
 )
