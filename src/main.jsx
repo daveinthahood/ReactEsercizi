447098db66bccd3b16575e8aa57472a50ff1ed22
daveinthahood <davidesimone96@outlook.it>
2023-08-08 11:23:18 +0200

    Login done

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ClickCounter } from './Counter'
+import {  Login } from './Login'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ClickCounter />
+  <Login />
 )
