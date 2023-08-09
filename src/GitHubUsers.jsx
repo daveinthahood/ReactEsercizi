0457ae1e68eb4c553fe0d9b4cecc9721c47e3658
daveinthahood <davidesimone96@outlook.it>
2023-08-09 17:29:31 +0200

    fix

--- src/GitHubUsers.jsx
+++ src/GitHubUsers.jsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react"
-
+import { useParams } from "react-router-dom"
 
 export const GitHubUsers = () => {
     const [data, setData] = useState([])
+    const {username} = useParams()
+   
 
 
     useEffect(() => {
         const fetchGitHub = async() =>{
             try {
-                const result = await fetch ("https://api.github.com/users/")
+                const result = await fetch ("https://api.github.com/users/${username}")
                 const res = await result.json()
                 setData(res)
             } catch (error) {
@@ -20,8 +22,7 @@ export const GitHubUsers = () => {
 
     return (
         <>
-
-           <p> User : {data.login} </p>
+           <p> User : {data && data.name} </p>
            <p> Id : {data.id}</p>
         </>
     )
