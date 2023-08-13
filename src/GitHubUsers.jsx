cfa3fb47ee7da4085358790c012184523f756a5f
daveinthahood <davidesimone96@outlook.it>
2023-08-13 13:26:53 +0200

    d

---
+++ src/GitHubUsers.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+export const GithubUser = () => {
+  const [data, setData] = useState(null);
+  const [err, setErr] = useState(null);
+  const { username } = useParams();
+
+  const fetchData = async () => {
+    if (!data) {
+      try {
+        const response = await fetch(
+          `https://api.github.com/users/${username}`
+        );
+        if (response.ok) {
+          const res = await response.json();
+          setData(res);
+        }
+      } catch (error) {
+        console.log(error);
+        setErr(error);
+      }
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <span>
+          {err ? err : `user: ${data && data.login}, id: ${data && data.id}`}
+        </span>
+      </div>
+    </>
+  );
+};
