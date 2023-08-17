c00879c0de0e64e40885a205a83e590fd39ef0ab
Dave <101344486+daveinthahood@users.noreply.github.com>
2023-08-17 15:12:26 +0200

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ShowGithubUser from './ShowUsers'
+import GithubUser from './GitHubUsers'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  
-    <ShowGithubUser />
+    <GithubUser />
   
 )
