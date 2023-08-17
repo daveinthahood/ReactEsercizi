4d9b250829341f1fbcb16cd3d49fdbe5c71627b2
Dave <101344486+daveinthahood@users.noreply.github.com>
2023-08-17 15:13:04 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import GithubUser from './GitHubUsers'
+import { GithubUser } from './GitHubUsers'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
